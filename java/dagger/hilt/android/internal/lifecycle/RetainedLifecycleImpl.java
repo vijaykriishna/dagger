8b3b370d47699aec4186518bc58d49b3901c47a9
Dagger Team <java-team-github-bot@google.com>
2024-06-10 13:20:12 -0700

    Internal change
    RELNOTES=n/a
    PiperOrigin-RevId: 642001855

--- java/dagger/hilt/android/internal/lifecycle/RetainedLifecycleImpl.java
+++ java/dagger/hilt/android/internal/lifecycle/RetainedLifecycleImpl.java
@@ -25,9 +25,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 /** Internal implementation. Do not use. */
-public final class RetainedLifecycleImpl
-    implements ActivityRetainedLifecycle,
-        ViewModelLifecycle {
+public final class RetainedLifecycleImpl implements ActivityRetainedLifecycle, ViewModelLifecycle {
 
   private final Set<RetainedLifecycle.OnClearedListener> listeners = new HashSet<>();
   private boolean onClearedDispatched = false;
