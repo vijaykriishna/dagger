972c8d35b7f132bf90ae94891fc9201aec2adb97
Brad Corso <bcorso@google.com>
2024-12-18 07:54:39 -0800

    Fix warnings about instantiating new boxed primitives.
    These constructors are deprecated for removal in upcoming java versions, and are currently giving the following warning:
    
    ```
    third_party/java_src/dagger/project/javatests/dagger/functional/basic/BasicTest.java:64: warning: [removal] Byte(byte) in Byte has been deprecated and marked for removal
        assertThat(basicComponent.getBoxedByte()).isEqualTo(new Byte(BOUND_BYTE));
    ```
    
    We only call these in a single test and AFAICT they aren't necessary or relevant to what this test is trying to assert so I'm removing them.
    
    RELNOTES=N/A
    PiperOrigin-RevId: 707550777

--- javatests/dagger/functional/basic/BasicTest.java
+++ javatests/dagger/functional/basic/BasicTest.java
@@ -61,23 +61,23 @@ public class BasicTest {
   }
 
   @Theory public void boxedPrimitives(BasicComponent basicComponent) {
-    assertThat(basicComponent.getBoxedByte()).isEqualTo(new Byte(BOUND_BYTE));
-    assertThat(basicComponent.getBoxedChar()).isEqualTo(new Character(BOUND_CHAR));
-    assertThat(basicComponent.getBoxedShort()).isEqualTo(new Short(BOUND_SHORT));
-    assertThat(basicComponent.getBoxedInt()).isEqualTo(new Integer(BOUND_INT));
-    assertThat(basicComponent.getBoxedLong()).isEqualTo(new Long(BOUND_LONG));
-    assertThat(basicComponent.getBoxedBoolean()).isEqualTo(new Boolean(BOUND_BOOLEAN));
+    assertThat(basicComponent.getBoxedByte()).isEqualTo(BOUND_BYTE);
+    assertThat(basicComponent.getBoxedChar()).isEqualTo(BOUND_CHAR);
+    assertThat(basicComponent.getBoxedShort()).isEqualTo(BOUND_SHORT);
+    assertThat(basicComponent.getBoxedInt()).isEqualTo(BOUND_INT);
+    assertThat(basicComponent.getBoxedLong()).isEqualTo(BOUND_LONG);
+    assertThat(basicComponent.getBoxedBoolean()).isEqualTo(BOUND_BOOLEAN);
     assertThat(basicComponent.getBoxedFloat()).isEqualTo(BOUND_FLOAT);
     assertThat(basicComponent.getBoxedDouble()).isEqualTo(BOUND_DOUBLE);
   }
 
   @Theory public void boxedPrimitiveProviders(BasicComponent basicComponent) {
-    assertThat(basicComponent.getByteProvider().get()).isEqualTo(new Byte(BOUND_BYTE));
-    assertThat(basicComponent.getCharProvider().get()).isEqualTo(new Character(BOUND_CHAR));
-    assertThat(basicComponent.getShortProvider().get()).isEqualTo(new Short(BOUND_SHORT));
-    assertThat(basicComponent.getIntProvider().get()).isEqualTo(new Integer(BOUND_INT));
-    assertThat(basicComponent.getLongProvider().get()).isEqualTo(new Long(BOUND_LONG));
-    assertThat(basicComponent.getBooleanProvider().get()).isEqualTo(new Boolean(BOUND_BOOLEAN));
+    assertThat(basicComponent.getByteProvider().get()).isEqualTo(BOUND_BYTE);
+    assertThat(basicComponent.getCharProvider().get()).isEqualTo(BOUND_CHAR);
+    assertThat(basicComponent.getShortProvider().get()).isEqualTo(BOUND_SHORT);
+    assertThat(basicComponent.getIntProvider().get()).isEqualTo(BOUND_INT);
+    assertThat(basicComponent.getLongProvider().get()).isEqualTo(BOUND_LONG);
+    assertThat(basicComponent.getBooleanProvider().get()).isEqualTo(BOUND_BOOLEAN);
     assertThat(basicComponent.getFloatProvider().get()).isEqualTo(BOUND_FLOAT);
     assertThat(basicComponent.getDoubleProvider().get()).isEqualTo(BOUND_DOUBLE);
   }
